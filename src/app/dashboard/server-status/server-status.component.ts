4be27fff19d418d5edee89556cdfc1e566c650cc
Yannick Bley <bleyyannickpro@gmail.com>
2024-08-11 07:58:18 +0200

    wip: add class binding for server status

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+
+
+interface ServerStatus {
+  status: 'offline' | 'online' | 'unknown';
+}
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   template:`
+        <div [class]="{
+         status: true, 
+         'status-online': currentStatus === 'online',
+         'status-offline': currentStatus === 'offline',
+         'status-unknown': currentStatus === 'unknown'
+        }">
           @if (currentStatus === 'online') {
           <p>Servers are online</p>
           <p>All systems are operational.</p>
@@ -21,13 +32,36 @@ import { Component } from '@angular/core';
   }, 
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit  {
+  
+  currentStatus: ServerStatus["status"] = 'online';
+  private interval?: ReturnType<typeof setTimeout>;
 
-  constructor() {
-    setInterval(()=> {
-      
-    })
+  constructor(private destroyRef: DestroyRef) {}
+
+  ngOnInit(): void {
+    this.interval = setInterval(()=> {
+      const random = Math.random();
+      if (random < 0.3) {
+        this.currentStatus = 'offline';
+      } else if (random < 0.6) {
+        this.currentStatus = 'online';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(this.interval);
+    });
   }
+
+ 
+  
+
+  
+
+  
+  
 
 }
